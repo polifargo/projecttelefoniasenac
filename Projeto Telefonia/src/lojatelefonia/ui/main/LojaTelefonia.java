fbabd26402999e3e1a86ee9b7ebe9dcf5fa676f4
Matheus <matheusnew_2009@hotmail.com>
2016-10-04 01:28:43 -0300

    Testando novamente.

--- Projeto Telefonia/src/lojatelefonia/ui/main/LojaTelefonia.java
+++ Projeto Telefonia/src/lojatelefonia/ui/main/LojaTelefonia.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package lojatelefonia.ui.main;
-//testando
+
 import lojatelefonia.ui.clientes.FormCadastrarCliente;
 import lojatelefonia.ui.clientes.FormConsultarCliente;
 import lojatelefonia.ui.produtos.FormCadastrarProduto;
